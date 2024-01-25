1754972508ea1d734a3a0da8a1376e372f05718d
crisTINACFG <cristina-gheorghe@outlook.com>
2024-01-25 20:45:18 +0000

    removed the weird supabase tutotial tsx thing

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-/*
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -14,33 +13,4 @@ export default function App() {
             </Stack.Navigator>
         </NavigationContainer>
     );
-}
-*/
-
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from './src/Supabase/Supabase'
-import Auth from './src/Supabase/Auth'
-import Account from './src/Supabase/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-
-export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-
-  return (
-    <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
-  )
-}
+};
